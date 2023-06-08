94b6cc2b2e5dfe2667fb5fdb2ce8fbceb217516a
Ekret <ikermesaperez@gmail.com>
2023-06-08 15:45:35 +0100

    Se entrega el ejercicio realizado 5  prueba falta comprobar el metodo a ver si esto funciona

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,36 +1,79 @@
+
+
 package cuentas;
 
+/**
+ * /**
+
+ * Esta clase define objetos que contienen tantos enteros aleatorios entre 0 y 1000 como se le definen al crear un objeto
+
+ * @author: Iker Mesa Pérez
+
+ * @version: 07/06/2023
+ 
+ * La clase CCuenta representa una cuenta bancaria con nombre, número de cuenta,
+ * saldo y tipo de interés.
+ */
 public class CCuenta {
-//primero cambiamos el nombre de la variable tipo a tipoInteres para que sea más claro a lo que nos estamos refiriendo
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInteres;
 
-
-
+    /**
+     * Crea una nueva instancia de CCuenta.
+     *
+     * @param nombre       El nombre del titular de la cuenta.
+     * @param cuenta       El número de cuenta.
+     * @param saldo        El saldo de la cuenta.
+     * @param tipoInterés  El tipo de interés de la cuenta.
+     */
     public CCuenta(String nombre, String cuenta, double saldo, double tipoInterés) {
-
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.saldo = saldo;
+        this.tipoInteres = tipoInterés;
     }
 
+    /**
+     * Devuelve el estado actual del saldo de la cuenta.
+     *
+     * @return El saldo de la cuenta.
+     */
     public double estado() {
         return saldo;
     }
 
+    /**
+     * Realiza un ingreso en la cuenta.
+     *
+     * @param cantidad  La cantidad a ingresar.
+     * @throws Exception Si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        saldo += cantidad;
     }
 
+    /**
+     * Realiza una retirada de dinero de la cuenta.
+     *
+     * @param cantidad  La cantidad a retirar.
+     * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo.
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
         if (estado() < cantidad)
             throw new Exception("No hay suficiente saldo");
-        saldo = saldo - cantidad;
+        saldo -= cantidad;
     }
 
+    /**
+     * Realiza una serie de operaciones en la cuenta.Se realiza un ingreso y una retirada de una cantidad fija.
+     * @param cantidad
+     */
     public void operativa_cuenta(double cantidad) {
         try {
             ingresar(cantidad);
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,6 +1,22 @@
 package cuentas;
 
+/**
+ * Clase que contiene el punto de entrada principal del programa.
+ 
+ * @author: Iker Mesa Pérez
+
+ * @version: 07/06/2023
+ 
+ * La clase CCuenta representa una cuenta bancaria con nombre, número de cuenta,
+ * saldo y tipo de interés.
+ */
 public class Main {
+
+    /**
+     * Realiza las operaciones en una cuenta específica.
+     *
+     * @param cuenta La cuenta en la que se realizarán las operaciones.
+     */
     public static void operativa_cuenta(CCuenta cuenta) {
         double saldoActual;
 
@@ -18,7 +34,7 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-        
+
         try {
             cuenta.ingresar(500);
             System.out.println("Ingreso en cuenta");
@@ -29,11 +45,16 @@ public class Main {
         }
     }
 
+    /**
+     * Punto de entrada principal del programa.
+     *
+     * @param args Los argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         CCuenta cuenta1;
 
         cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        
+
         operativa_cuenta(cuenta1);
     }
 }
